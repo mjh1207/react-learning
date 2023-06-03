3d925645a0bc4bd032ed86d13a6ba2b0db7c10a0
Mitchell Hernandez <mitchh1207@gmail.com>
2023-06-03 12:57:08 -0600

    Section 3.36

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,21 @@
+import "./ExpenseItem.css";
+
+function ExpenseItem(props) {
+  return (
+    <div className="expense-item">
+      <div>
+        {props.date.toLocaleString("en-us", {
+          month: "long",
+          day: "numeric",
+          year: "numeric",
+        })}
+      </div>
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
+      </div>
+    </div>
+  );
+}
+
+export default ExpenseItem;
