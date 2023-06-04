d7683a1d095cc5e375b92bb8450d649834b2a49b
Mitchell Hernandez <mitchh1207@gmail.com>
2023-06-04 12:39:49 -0600

    onClick event handler

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -2,12 +2,13 @@ import "./Expenses.css";
 import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
 
-function Expenses(props) {
+const Expenses = (props) => {
   return (
     <Card className="expenses">
       {props.expenses.map((expense) => {
         return (
           <ExpenseItem
+            key={expense.id}
             title={expense.title}
             amount={expense.amount}
             date={expense.date}
@@ -16,6 +17,6 @@ function Expenses(props) {
       })}
     </Card>
   );
-}
+};
 
 export default Expenses;
