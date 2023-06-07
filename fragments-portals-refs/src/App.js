7bd5c37cf138ce8ea47e74def3b4e21ed0d1faa7
Mitchell Hernandez <mitchh1207@gmail.com>
2023-06-07 15:55:46 -0600

    Fragment examples

--- fragments-portals-refs/src/App.js
+++ fragments-portals-refs/src/App.js
@@ -2,7 +2,6 @@ import React, { useState } from "react";
 
 import AddUser from "./components/Users/AddUser";
 import UsersList from "./components/Users/UsersList";
-import Wrapper from "./components/Helpers/Wrapper";
 
 function App() {
   const [usersList, setUsersList] = useState([]);
@@ -17,10 +16,10 @@ function App() {
   };
 
   return (
-    <Wrapper>
+    <>
       <AddUser onAddUser={addUserHandler} />
       <UsersList users={usersList} />
-    </Wrapper>
+    </>
   );
 }
 
--- fragments-portals-refs/src/components/UI/ErrorModal.js
+++ fragments-portals-refs/src/components/UI/ErrorModal.js
@@ -1,12 +1,12 @@
-import React from 'react';
+import React, { Fragment } from "react";
 
-import Card from './Card';
-import Button from './Button';
-import classes from './ErrorModal.module.css';
+import Card from "./Card";
+import Button from "./Button";
+import classes from "./ErrorModal.module.css";
 
 const ErrorModal = (props) => {
   return (
-    <div>
+    <Fragment>
       <div className={classes.backdrop} onClick={props.onConfirm} />
       <Card className={classes.modal}>
         <header className={classes.header}>
@@ -19,7 +19,7 @@ const ErrorModal = (props) => {
           <Button onClick={props.onConfirm}>Okay</Button>
         </footer>
       </Card>
-    </div>
+    </Fragment>
   );
 };
 
